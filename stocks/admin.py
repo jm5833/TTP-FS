171dfc7c6c34b954bdb6352b118974cbf41ae68d
Jack Martinez <jm5833@nyu.edu>
2019-08-08 18:48:17 -0400

    added created tables to django's admin page

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Portfolio, Transactions
 
-# Register your models here.
+#registering created models so that they show up on the admin page
+admin.site.register(Profile)
+admin.site.register(Portfolio)
+admin.site.register(Transactions)
