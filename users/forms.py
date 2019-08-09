6b222c5c68eb73e9de57b62b7e5e69593e36f342
Jack Martinez <jm5833@nyu.edu>
2019-08-09 12:51:17 -0400

    Implemented custom user registration form

---
+++ users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+#create a custom usercreationform that also accepts name and email
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 
+            'email',
+            'username',
+            'password1',
+            'password2'
+        )
+
+    #overwrite save function to save the additional information
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.name = self.cleaned_data['name']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
--- users/views.py
+++ users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from users.forms import RegisterForm
 
 def register(request):
     #check if user has already submitted data to the register form 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         #obtain username if the submitted data is valid
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('stocks-home')
     else:
-        #use django's builtin signup form
-        form = UserCreationForm()
+        #use created registration form
+        form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
