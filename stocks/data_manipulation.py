79c15fa5ce49fc3406a7a5e4c1b18c10de79d5d3
Jack Martinez <jm5833@nyu.edu>
2019-08-13 13:33:04 -0400

    Portfolio table updates correctly when purchasing stocks for an already owned stock

--- stocks/data_manipulation.py
+++ stocks/data_manipulation.py
@@ -15,16 +15,21 @@ def save_transaction(request, form):
 
 #function to add a stock to the portfolio table
 def update_portfolio(request, form):
-    stock_ticker = form.cleaned_data['stock_ticker']
     user = request.user
+    stock_ticker = form.cleaned_data['stock_ticker']
+    num_of_shares = form.cleaned_data['num_of_shares']
     price = get_stock_data(stock_ticker).get('price')
     #retrieve the number of stocks owned by the user for s_ticker
     user_stock = Portfolio.objects.filter(user=user, stock_ticker=stock_ticker)
     #if the user has no stocks in that company, add it to their portfolio
     if len(user_stock) == 0:
-        num_of_shares = form.cleaned_data['num_of_shares']
         add_portfolio(user, stock_ticker, num_of_shares, price)
-
+    #compound stock quantity if the user already owns stock in said company
+    else:
+        p = user_stock[0]
+        p.num_of_shares += num_of_shares
+        p.save()
+            
 #function to add an entry into the portfolio table
 def add_portfolio(user, stock_ticker, num_of_shares, price):
     p = Portfolio(
